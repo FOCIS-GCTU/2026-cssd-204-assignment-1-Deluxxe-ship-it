e389687645dddadda60d9b03aa327d2d8fd652fe
Deluxxe-ship-it <kkessieboateng@gmail.com>
2026-05-31 05:12:23 -0800

    Assign.1 initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,27 @@
-
-def main():
-  pass #code goes here
-
-
-
+#File:initials.py
+#Description: Print out my initials.
+#Assignment Number:1
+#
+#Name: Kessie-Boateng Kwabena
+#STUDENT ID: 2425404394
+#Email: 2425404394@live.gctu.edu.gh
+#Grader: Augustus Buckman
+#
+#On my honor, Kessie-Boateng Kwabena, this programming assignment is my own work
+#and i have not provided this code to any other student.
+print("  ")
+def main(): # A python code to print out my initials K.B.K
+    print("...KBK")
+    print("  ")
+    print("...KK........KK........BBBBBBBBBB..........KK........KK.....")
+    print("...KK.......KK.........BB.........B........KK.......KK......")
+    print("...KK......KK..........BB.........B........KK......KK.......")
+    print("...KK.....KK...........BB........B.........KK.....KK........")
+    print("...KK....KK............BBBBBBBBB...........KK....KK.........")
+    print("...KK.KK...............BB........B.........KK.KK............")
+    print("...KK...KK.............BB.........B........KK...KK..........")
+    print("...KK......KK..........BB.........B........KK......KK.......")
+    print("...KK........KK...**...BB.........B...**...KK........KK...**")
+    print("...KK........KK...**...BBBBBBBBBB.....**...KK........KK...**")
+    print("  ")
 main()
